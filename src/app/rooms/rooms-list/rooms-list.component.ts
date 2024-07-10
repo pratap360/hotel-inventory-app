be0e1949e1c63e9f880a0f0e1f4ecb7374f195a5
pratap360 <pratapparui0360@gmail.com>
2024-07-11 00:28:50 +0530

    added room-list componet

---
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { RoomList } from '../room';
+
+@Component({
+  selector: 'hoin-rooms-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './rooms-list.component.html',
+  styleUrl: './rooms-list.component.scss'
+})
+export class RoomsListComponent implements OnInit {
+
+  @Input() rooms: RoomList[] = [];
+   
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+}
