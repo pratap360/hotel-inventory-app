0b2a447e02b3eb9cf71d16a88188472d499641e1
pratap360 <pratapparui0360@gmail.com>
2024-08-23 22:03:58 +0530

    added imports in rooms module & route

--- src/app/rooms/rooms.module.ts
+++ src/app/rooms/rooms.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RoomsRoutingModule } from './rooms-routing.module';
+import { RoomsComponent } from './rooms.component';
+import { RoomsListComponent } from './rooms-list/rooms-list.component';
+import { RoomsBookingComponent } from './rooms-booking/rooms-booking.component';
+import { RoomsAddComponent } from './rooms-add/rooms-add.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RoomsRoutingModule
+    RoomsRoutingModule,
+    RoomsComponent,
+    RoomsListComponent,
+    RoomsBookingComponent,
+    RoomsAddComponent
   ]
 })
 export class RoomsModule { }
