9d6851928329aaeb0442bb2c096d1f397d91636b
pratap360 <pratapparui0360@gmail.com>
2024-08-25 16:23:34 +0530

    Introduction of Reactive Forms  & Using Material Controls

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,17 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigsService } from '../services/configs.service';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @Component({
   selector: 'hoin-booking',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule, MatInputModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule],
   templateUrl: './booking.component.html',
-  styleUrl: './booking.component.scss'
+  styleUrl: './booking.component.scss',
 })
 export class BookingComponent implements OnInit {
-  constructor(private configsService: ConfigsService) { }
+  bookingForm!: FormGroup;
+
+  constructor(
+    private configsService: ConfigsService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.bookingForm = this.fb.group({
+      roomId: new FormControl(''),
+      bookingId: [''],
+      guestEmail: [''],
+      checkinDate: [''],
+      checkoutDate: [''],
+      bookingStatus: [''],
+      bookingAmount: [''],
+      bookingDate: [''],
+      mobileNumber: [''],
+      guestName: [''],
+      guestAddress: [''],
+      guestCity: [''],
+      guestState: [''],
+      guestCountry: [''],
+      guestZipCode: [''],
+      guestCount: [''],
+      guestList: [''],
+    });
   }
-
 }
