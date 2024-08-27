a902ecf29b71fd3e6be8fbe24594d7368173bbd0
pratap360 <pratapparui0360@gmail.com>
2024-08-27 23:39:48 +0530

    Dynamic Forms remaining part

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,29 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfigsService } from '../services/configs.service';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
-import {MatIconModule} from '@angular/material/icon';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'hoin-booking',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule, MatInputModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule,MatButtonModule,MatAccordion,MatExpansionModule,MatIconModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatAccordion,
+    MatExpansionModule,
+    MatIconModule,
+  ],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.scss',
 })
 export class BookingComponent implements OnInit {
   bookingForm!: FormGroup;
+  i: any;
 
   get guests() {
     return this.bookingForm.get('guests') as FormArray;
-  } 
+  }
 
   constructor(
     private configsService: ConfigsService,
@@ -32,31 +49,30 @@ export class BookingComponent implements OnInit {
 
   ngOnInit(): void {
     this.bookingForm = this.fb.group({
-      roomId: new FormControl({value:'12',disabled: true}),
+      roomId: new FormControl({ value: '12', disabled: true }),
       bookingId: [''],
       guestEmail: [''],
       checkinDate: [''],
       checkoutDate: [''],
 
-      address : this.fb.group({
-        addressLine1: [''], 
-        addressLine2: [''], 
+      address: this.fb.group({
+        addressLine1: [''],
+        addressLine2: [''],
         city: [''],
         state: [''],
         country: [''],
         zipCode: [''],
       }),
-      
+
       bookingStatus: [''],
       bookingAmount: [''],
       bookingDate: [''],
       mobileNumber: [''],
       guestName: [''],
-     
 
-      guests: this.fb.array ([this.fb.group({
-        guestName: [''], age: new FormControl ('')
-      })]),
+      guests: this.fb.array([
+        this.addGuestControl(),
+      ]),
       guestList: [''],
     });
   }
@@ -68,8 +84,24 @@ export class BookingComponent implements OnInit {
 
   addGuest() {
     this.guests.push(
-      this.fb.group({guestName: [''], age: new FormControl ('')
-    }));
+      this.addGuestControl()
+    );
   }
 
+  addGuestControl() {
+    return this.fb.group({ guestName: [''], age: new FormControl('') })
+  }
+
+  addPassport() {
+    this.bookingForm.addControl('passport', new FormControl(''));
+  }
+  deletePassport() {
+    if (this.bookingForm.get('passport')) {
+      this.bookingForm.removeControl('passport');
+    }
+  }
+
+  removeGuest(i: number) {
+    this.guests.removeAt(i);
+  }
 }
